fdf168a6dca41a23352bad148fb6fd56c32a2bde
Jonas Miosga <jonas.miosga@uni-konstanz.de>
2020-09-01 15:20:53 +0200

    OLS added/R squared calc.

--- moto_scrape.py
+++ moto_scrape.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
 
 path = "/Users/jonasmiosga/Desktop/motorcycle_analysis/mo_re_01092020.txt"
 
@@ -16,10 +19,12 @@ b = soup.find_all("span", class_="h3 u-block")
 ez = []
 for i in range(0,len(a)):
     if a[i].get_text()[0:3] == "Neu":
-        ez.append("Neu")
+        ez.append(0)
     else:
         c = re.search(r"\d{2}(\D)\d{4}",a[i].get_text())
-        ez.append(c.group(0))
+        age = datetime.today() - datetime.strptime(c.group(0),'%m/%Y')
+        ez.append(age.days)
+ez = [int(i) for i in ez]
 
 # mileage in km
 km = []
@@ -30,6 +35,7 @@ for i in range(0,len(a)):
         c = re.search(r"\,\s(.*)\skm",a[i].get_text())
         km.append(c.group(1))
 km = [i.replace(".","") for i in km]
+km = [int(i) for i in km]
 
 # clean prices
 price = []
@@ -38,4 +44,8 @@ for i in range(0,len(b)):
     price.append(int(c[0]+c[2:5]))
 
 df = pd.DataFrame({'ez': ez, 'price': price, 'km': km})
-print(df)
+
+# ols
+X = np.column_stack([ez,km])
+reg = LinearRegression().fit(X, price)
+print(reg.score(X,price))
